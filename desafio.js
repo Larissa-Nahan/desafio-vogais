9019a0daa712456bc4e4fdcf019a1f8e25b6a438
Larissa-Nahan <larissanahan@gmail.com>
2022-05-24 20:36:05 -0300

    Adicionando acentuação

--- desafio.js
+++ desafio.js
@@ -1,23 +1,22 @@
 const text1 = "eu vou para a escola";
 const text2 = "o dia esta lindo hoje";
+const text3 = "Ela é a vilã";
+
+const vogais = "aàáãâeèéêiìíîoòóõôuùúû"
 
 function contaVogais(texto) {
   let quant = 0;
   texto = texto.replaceAll(" ", "");
   texto = texto.toLowerCase();
 
-  for (letra of texto) {
-    if (
-      letra == "a" ||
-      letra == "e" ||
-      letra == "i" ||
-      letra == "o" ||
-      letra == "u"
-    )
-      quant++;
+  
+  for (let i = 0; i < vogais.length; i++) {
+    for (let j = 0; j < texto.length; j++) {
+      if (vogais[i] === texto[j]) quant++;
+    }
   }
 
   console.log(quant);
 }
 
-contaVogais(text1);
+contaVogais(text3);
